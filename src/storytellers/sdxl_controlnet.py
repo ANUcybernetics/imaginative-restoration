16f041eb55e298cef9fcd2a2b896cfdc92a66be8
Ben Swift <ben@benswift.me>
2024-09-16 16:56:59 +1000

    add sdxl_controlnet module
    not even sure if it works yet, because I don't have a cuda box to test on

---
+++ src/storytellers/sdxl_controlnet.py
@@ -0,0 +1,45 @@
+import storytellers.image as image
+from diffusers import (
+    ControlNetModel,
+    StableDiffusionXLControlNetPipeline,
+    AutoencoderKL,
+)
+import torch
+
+controlnet = ControlNetModel.from_pretrained(
+    "diffusers/controlnet-canny-sdxl-1.0", torch_dtype=torch.float16
+)
+vae = AutoencoderKL.from_pretrained(
+    "madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16
+)
+pipe = StableDiffusionXLControlNetPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0",
+    controlnet=controlnet,
+    vae=vae,
+    torch_dtype=torch.float16,
+)
+pipe.enable_model_cpu_offload()
+# pipe.to("mps")
+pipe.set_progress_bar_config(disable=True)
+
+
+def predict(init_image, prompt, size, strength, steps, seed=1231231):
+    negative_prompt = "low quality, bad quality, sketches"
+
+    # init_image = load_image(
+    #     "https://huggingface.co/datasets/hf-internal-testing/diffusers-images/resolve/main/sd_controlnet/hf-logo.png"
+    # )
+
+    init_image = image.resize_crop(init_image, size)
+    controlnet_conditioning_scale = 0.5  # recommended for good generalization
+
+    canny_image = image.canny_image(init_image)
+
+    results = pipe(
+        prompt,
+        negative_prompt=negative_prompt,
+        image=canny_image,
+        controlnet_conditioning_scale=controlnet_conditioning_scale,
+    )
+
+    return results.images[0]
