1a99c95a54ae415c207e836d26ad25fce307469e
Ben Swift <ben@benswift.me>
2024-10-09 09:46:36 +1100

    crop frames to square (for SD reasons)

--- assets/nfsa/reprocess-video.sh
+++ assets/nfsa/reprocess-video.sh
@@ -3,13 +3,17 @@ set -euo pipefail
 IFS=$'\n\t'
 
 VIDEO_FILE=IMGRES_FirstRoughEdit_V1.0_DH_11.09.24.mp4
-FRAME_WIDTH=256
+FRAME_WIDTH=512
 
 # remove all existing frames
 rm -f frame-*.png
 
 # generate a bunch of new frames
-ffmpeg -i $VIDEO_FILE -vf "crop=$FRAME_WIDTH:$FRAME_WIDTH*ih/iw:0:0,scale=$FRAME_WIDTH:-1" -vsync 0 frame-%04d.png
+# crop square from middle of input video
+ffmpeg -i $VIDEO_FILE -vf "crop=in_h:in_h:(in_w-in_h)/2:0,scale=$FRAME_WIDTH:$FRAME_WIDTH" -vsync 0 frame-%04d.png
+
+# original command (commented out)
+# ffmpeg -i $VIDEO_FILE -vf "crop=ih:ih,scale=$FRAME_WIDTH:$FRAME_WIDTH" -vsync 0 frame-%04d.png
 
 # extract audio from video (wav to minimize futzing with gstreamer plugins)
 ffmpeg -y -i $VIDEO_FILE -vn -acodec pcm_s16le -ar 44100 -ac 2 audio.wav
