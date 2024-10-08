ef0e952c62753b62badef93a1cb0830e707688f5
Ben Swift <ben@benswift.me>
2024-10-08 17:27:11 +1100

    fix some duplicated code (accidentally commited)

--- src/storytellers/main_window.py
+++ src/storytellers/main_window.py
@@ -70,28 +70,6 @@ class MainWindow(QMainWindow):
         self.audio_player.setLoopCount(QSoundEffect.Infinite)
         if not self.audio_player.play():
             QMessageBox.warning(self, "Audio Error", "Failed to play audio. Check your audio setup.")
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Storytellers")
-        self.showFullScreen()
-
-        self.image_label = QLabel(self)
-        self.image_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.image_label.setAlignment(Qt.AlignCenter)
-        self.setCentralWidget(self.image_label)
-
-        self.frame_index = 1
-        self.ai_frame, _ = utils.get_film_frame(self.frame_index)
-
-        self.ai_worker = AIFrameWorker()
-        self.ai_worker.frame_ready.connect(self.on_ai_frame_ready)
-        self.ai_worker.start()
-
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.update_frame)
-        self.timer.start(int(utils.FRAME_TIME * 1000))  # Convert to milliseconds
-
-        self.setFocusPolicy(Qt.StrongFocus)
 
     @Slot(object)
     def on_ai_frame_ready(self, new_ai_frame):
