027a60aa96f5a7bf1563259d5539255e76c868d7
Ben Swift <ben@benswift.me>
2024-10-22 15:54:08 +1100

    add "letterboxing" for NFSA film
    there's an issue where the webcam view starts in the middle and "jumps" to
    the right spot when the video starts. Will fix later.

--- lib/imaginative_restoration_web/live/index_live.ex
+++ lib/imaginative_restoration_web/live/index_live.ex
@@ -8,20 +8,22 @@ defmodule ImaginativeRestorationWeb.IndexLive do
 
   def render(assigns) do
     ~H"""
-    <div class="w-full aspect-[4/3]">
-      <video autoplay loop muted class="size-full">
-        <source
-          src="https://fly.storage.tigris.dev/imaginative-restoration-sketches/IMGRES_FirstRoughEdit_V1.0_DH_11.09.24.mp4"
-          type="video/mp4"
-        /> Your browser does not support the video tag.
-      </video>
-      <video
-        id="video"
-        phx-hook="WebcamStream"
-        class="absolute top-8 right-8 size-[240px] object-cover"
-      >
-        Video stream not available.
-      </video>
+    <div class=" flex items-center justify-center h-full">
+      <div class="relative w-full max-w-[calc(100vh*4/3)] aspect-[4/3]">
+        <video autoplay loop muted class="w-full h-full object-contain">
+          <source
+            src="https://fly.storage.tigris.dev/imaginative-restoration-sketches/IMGRES_FirstRoughEdit_V1.0_DH_11.09.24.mp4"
+            type="video/mp4"
+          /> Your browser does not support the video tag.
+        </video>
+        <video
+          id="video"
+          phx-hook="WebcamStream"
+          class="absolute top-8 right-8 size-[240px] object-cover"
+        >
+          Video stream not available.
+        </video>
+      </div>
     </div>
     """
   end
