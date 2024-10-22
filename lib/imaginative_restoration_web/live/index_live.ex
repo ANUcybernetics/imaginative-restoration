8e363bcf028ba10beb3260cb866854bd770c6384
Ben Swift <ben@benswift.me>
2024-10-22 15:45:29 +1100

    more tweaks to the layout of video elements
    still searching for the letterbox, though

--- lib/imaginative_restoration_web/live/index_live.ex
+++ lib/imaginative_restoration_web/live/index_live.ex
@@ -8,16 +8,18 @@ defmodule ImaginativeRestorationWeb.IndexLive do
 
   def render(assigns) do
     ~H"""
-    <div class="w-full">
-      <video autoplay loop muted class="w-full h-full object-cover">
+    <div class="w-full aspect-[4/3]">
+      <video autoplay loop muted class="size-full">
         <source
           src="https://fly.storage.tigris.dev/imaginative-restoration-sketches/IMGRES_FirstRoughEdit_V1.0_DH_11.09.24.mp4"
           type="video/mp4"
         /> Your browser does not support the video tag.
       </video>
-    </div>
-    <div class="absolute top-8 right-8">
-      <video id="video" phx-hook="WebcamStream" class="size-[240px] object-cover">
+      <video
+        id="video"
+        phx-hook="WebcamStream"
+        class="absolute top-8 right-8 size-[240px] object-cover"
+      >
         Video stream not available.
       </video>
     </div>
