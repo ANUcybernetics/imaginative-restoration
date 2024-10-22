c26ba28d1a902ad28cbff6d7cc33e31f63c7ad30
Ben Swift <ben@benswift.me>
2024-10-22 15:22:41 +1100

    play the (rough cut) NFSA video in the main view

--- lib/imaginative_restoration_web/live/index_live.ex
+++ lib/imaginative_restoration_web/live/index_live.ex
@@ -8,6 +8,14 @@ defmodule ImaginativeRestorationWeb.IndexLive do
 
   def render(assigns) do
     ~H"""
+    <div class="w-full">
+      <video autoplay loop muted class="w-full h-full object-cover">
+        <source
+          src="https://fly.storage.tigris.dev/imaginative-restoration-sketches/IMGRES_FirstRoughEdit_V1.0_DH_11.09.24.mp4"
+          type="video/mp4"
+        /> Your browser does not support the video tag.
+      </video>
+    </div>
     <div class="absolute top-8 right-8">
       <video id="video" phx-hook="WebcamStream" class="size-[240px] object-cover">
         Video stream not available.
