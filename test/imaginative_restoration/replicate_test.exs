6398a5e132aaca3310f7979bdb86671759ddbd35
Ben Swift <ben@benswift.me>
2024-10-23 10:44:12 +1100

    add replicate tests
    they don't pass yet

---
+++ test/imaginative_restoration/replicate_test.exs
@@ -0,0 +1,73 @@
+defmodule ImaginativeRestoration.ReplicateTest do
+  use ImaginativeRestoration.DataCase
+
+  alias ImaginativeRestoration.AI.Replicate
+
+  defp invoke_args do
+    sketch_image = "https://fly.storage.tigris.dev/imaginative-restoration-sketches/butterfly-sketch.png"
+    prompt = "fauvism, matisse, cave painting"
+
+    [
+      ["adirik/t2i-adapter-sdxl-sketch", sketch_image, prompt],
+      ["lucataco/florence-2-large", sketch_image],
+      ["lucataco/remove-bg", sketch_image]
+    ]
+  end
+
+  describe "Replicate platform" do
+    @describetag timeout: :timer.minutes(10)
+
+    test "can list latest model version for all models" do
+      for [model | _] <- invoke_args() do
+        assert {:ok, version} = Replicate.get_latest_version(model)
+        assert String.match?(version, ~r/^[a-f0-9]{64}$/)
+      end
+    end
+
+    # @tag skip: true
+    test "can successfully invoke all Replicate models" do
+      tasks =
+        for [model | _] = args <- invoke_args() do
+          Task.async(fn ->
+            result = apply(Replicate, :invoke, args)
+
+            case result do
+              {:ok, output} ->
+                IO.puts("#{model} output: #{output}")
+
+              _ ->
+                :pass
+            end
+
+            {model, result}
+          end)
+        end
+
+      results = Task.await_many(tasks, :infinity)
+
+      failed_models =
+        results
+        |> Enum.filter(fn {_, result} ->
+          case result do
+            {:error, _} -> true
+            _ -> false
+          end
+        end)
+        |> Enum.map(fn {model, _} -> model end)
+
+      if length(failed_models) > 0 do
+        IO.puts("Failed models: #{Enum.join(failed_models, ", ")}")
+      end
+
+      for {_, result} <- results do
+        case result do
+          {:ok, output} ->
+            assert String.match?(output, ~r/\S/)
+
+          {:error, _} ->
+            flunk("Some models failed to invoke")
+        end
+      end
+    end
+  end
+end
