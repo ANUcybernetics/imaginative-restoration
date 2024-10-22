b417835a11a907c482aceea39b91868445e43286
Ben Swift <ben@benswift.me>
2024-10-22 15:18:43 +1100

    x-flip the video stream

--- assets/js/webcam_stream_hook.js
+++ assets/js/webcam_stream_hook.js
@@ -10,6 +10,8 @@ const WebcamStreamHook = {
       .getUserMedia({ video: true, audio: false })
       .then((stream) => {
         video.srcObject = stream;
+        // flip video horizontally (useful for normal webcam use, not necessarily for overhead setup)
+        video.style.transform = "scaleX(-1)";
         video.play();
       })
       .catch((error) => {
