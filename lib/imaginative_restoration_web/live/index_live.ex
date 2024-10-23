0c5393956f2634aab30f20eacfe9e113e636d770
Ben Swift <ben@benswift.me>
2024-10-24 08:45:19 +1100

    fix bug: need to capture self() outside the task fn
    also add missing @impl trues

--- lib/imaginative_restoration_web/live/index_live.ex
+++ lib/imaginative_restoration_web/live/index_live.ex
@@ -4,6 +4,7 @@ defmodule ImaginativeRestorationWeb.IndexLive do
 
   require Logger
 
+  @impl true
   def render(assigns) do
     ~H"""
     <div class=" flex items-center justify-center h-full">
@@ -29,23 +30,29 @@ defmodule ImaginativeRestorationWeb.IndexLive do
     """
   end
 
+  @impl true
   def mount(_params, _session, socket) do
     {:ok, assign(socket, sketch_dataurl: nil, processed_dataurl: nil)}
   end
 
+  @impl true
   def handle_event("webcam_frame", %{"frame" => dataurl}, socket) do
+    pid = self()
+
     Task.start(fn ->
       {:ok, sketch} = ImaginativeRestoration.Sketches.process(dataurl)
-      send(self(), {:processed_frame, sketch.processed})
+      send(pid, {:processed_frame, {:ok, sketch.processed}})
     end)
 
     {:noreply, assign(socket, sketch_dataurl: dataurl)}
   end
 
+  @impl true
   def handle_info({:processed_frame, {:ok, processed_dataurl}}, socket) do
     {:noreply, assign(socket, processed_dataurl: processed_dataurl)}
   end
 
+  @impl true
   def handle_info({:processed_frame, {:error, reason}}, socket) do
     Logger.warning("Processing failed: #{inspect(reason)}")
     {:noreply, socket}
