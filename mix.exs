7ff51a855812875f355fd19faee38acd6dd89254
Ben Swift <ben@benswift.me>
2024-10-31 16:38:17 +1100

    set project (and min Elixir) version in mix.exs

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule ImaginativeRestoration.MixProject do
   def project do
     [
       app: :imaginative_restoration,
-      version: "0.1.0",
-      elixir: "~> 1.14",
+      version: "1.0.0",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
