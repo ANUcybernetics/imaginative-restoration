bc63ced272a4e83fb829afe4c77deee82b71308e
Ben Swift <ben@benswift.me>
2024-11-20 17:28:55 +1100

    don't require user gesture for autoplay

--- launch-chrome.sh
+++ launch-chrome.sh
@@ -3,6 +3,7 @@ open -a "Google Chrome" --new --args \
     --new-window \
     --auth-server-whitelist="imgres.fly.dev" \
     --auth-negotiate-delegate-whitelist="imgres.fly.dev" \
+    --autoplay-policy=no-user-gesture-required \
     --start-fullscreen \
     "http://imgres.fly.dev?capture_box=70,90,470,300"
 
@@ -15,5 +16,6 @@ open -a "Google Chrome" --new --args \
     --new-window \
     --auth-server-whitelist="imgres.fly.dev" \
     --auth-negotiate-delegate-whitelist="imgres.fly.dev" \
+    --autoplay-policy=no-user-gesture-required \
     --start-fullscreen \
     "http://imgres.fly.dev"
