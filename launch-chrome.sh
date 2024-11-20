9a155d92987e9997390a692cc2d81f9357a7b742
Ben Swift <ben@benswift.me>
2024-11-20 17:00:02 +1100

    add launch-chrome.sh shell script

---
+++ launch-chrome.sh
@@ -0,0 +1,8 @@
+# Open Chrome on first monitor
+open -a "Google Chrome" --new --args --new-window --start-fullscreen "http://imgres.fly.dev?capture_box=70,90,470,300"
+
+# Wait a moment to ensure first window is opened
+sleep 2
+
+# Open Chrome on second monitor
+open -a "Google Chrome" --new --args --new-window --start-fullscreen "http://imgres.fly.dev"
