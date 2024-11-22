cb13f4a266c7677a8755cc99e6425e42ad656db7
Ben Swift <ben@benswift.me>
2024-11-22 15:50:00 +1100

    try and set both windows to fullscreen

--- launch-chrome.sh
+++ launch-chrome.sh
@@ -15,13 +15,13 @@ open -a "Google Chrome" --new --args \
     "http://$IMGRES_AUTH@imgres.fly.dev?capture_box=70,90,470,300"
 
 # Wait for first window to open
-sleep 3
+sleep 5
 
 # Open second Chrome window and position it on second monitor using AppleScript
 osascript <<EOF
 tell application "Google Chrome"
     make new window
-    set bounds of front window to [2560, 0, 5120, 1440] # Adjust these coordinates based on your monitor setup
+    set bounds of front window to [2560, 200, 3560, 800] # should be on second monitor
     tell front window
         set URL of active tab to "http://$IMGRES_AUTH@imgres.fly.dev"
     end tell
@@ -29,9 +29,17 @@ end tell
 EOF
 
 # Set the second window to fullscreen mode
-sleep 2
+sleep 5
 osascript <<EOF
 tell application "System Events"
-    keystroke "f" using {command down, control down}
+    tell application "Google Chrome"
+        # Set first window to fullscreen
+        set index of first window to 1
+        keystroke "f" using {command down, control down}
+
+        # Set second window to fullscreen
+        set index of second window to 1
+        keystroke "f" using {command down, control down}
+    end tell
 end tell
 EOF
